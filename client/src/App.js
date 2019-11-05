4bd055a14fa270da097b121f3146ef2ef6c7d5b8
John C <conjohnerton@gmail.com>
2019-11-05 11:04:26 -0500

    added template

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Login from "./components/Login";
 import "./App.css";
 
-function App() {
-   return <div className="App">Hello, Yoannier!</div>;
-}
+const App = () => {
+   return (
+      <div className="App">
+         <Login />
+      </div>
+   );
+};
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,6 +10,7 @@ import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
