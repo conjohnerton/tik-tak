b5f8b9b8e80e0322559731bba3d722ef7165d168
John C <conjohnerton@gmail.com>
2019-11-06 21:19:03 -0500

    Get local yaks route

--- routes/api/yaks.js
+++ routes/api/yaks.js
@@ -5,9 +5,22 @@ const auth = require("../../middleware/auth");
 // Imported Schemas
 const Comment = require("../../models/User");
 
-//  * 5 Miles === 8046.72 Meters
 router.get("/yaks", auth, async (req, res) => {
-   res.json({ msg: "hello" });
+   try {
+      // Get yaks around given location, measured along the surface of Earth
+      const localYakList = await Yak.geoNear(
+         {
+            type: "Point",
+            coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+         },
+         { maxDistance: 8046.72, spherical: true } // 5 miles
+      );
+
+      // Return found yaks
+      res.status(200).json({ success: true, yaks: localYakList });
+   } catch (exception) {
+      res.json(exception);
+   }
 });
 
 module.exports = router;
