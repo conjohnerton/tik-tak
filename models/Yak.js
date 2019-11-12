040cd6f6c25535e2c129c661397e7f8853b2d43b
John C <conjohnerton@gmail.com>
2019-11-12 18:40:58 -0500

    formatting commit

--- models/Yak.js
+++ models/Yak.js
@@ -1,32 +1,34 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// TODO: Get the
+
 // The model for a location that maps on the surface of the Earth.
 const GeoSchema = new Schema({
-   type: {
-      type: String,
-      default: "Point"
-   },
-   coordinates: {
-      type: [Number],
-      index: "2dsphere"
-   }
+  type: {
+    type: String,
+    default: "Point"
+  },
+  coordinates: {
+    type: [Number],
+    index: "2dsphere"
+  }
 });
 
 // The model that represents every post, ever.
 const YakSchema = new Schema({
-   content: { type: String, required: true },
-   // author: { type: Schema.Types.ObjectId, ref: "User" },
+  content: { type: String, required: true },
+  // author: { type: Schema.Types.ObjectId, ref: "User" },
 
-   // TODO: This should be used in sprint2
-   // comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  // TODO: This should be used in sprint2
+  // comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
 
-   // TODO: CREATE IMAGE FIELD
+  // TODO: CREATE IMAGE FIELD
 
-   geometry: GeoSchema,
+  geometry: GeoSchema,
 
-   // This may need a different expiration time for testing purpose
-   createdAt: { type: Date, expires: "24h", default: Date.now } // one of these
+  // This may need a different expiration time for testing purpose
+  createdAt: { type: Date, expires: "24h", default: Date.now } // one of these
 });
 
 YakSchema.index({ geometry: "2dsphere" });
