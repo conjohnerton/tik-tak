6b001b6d5d87498aee860271bf7e25e2fca654ef
John C <conjohnerton@gmail.com>
2019-11-12 20:35:52 -0500

    Delete Yaks

--- routes/api/yaks.js
+++ routes/api/yaks.js
@@ -10,56 +10,78 @@ const User = require("../../models/User");
 
 // Gets yaks within a 5 mile radius of input location
 router.get("/", auth, async (req, res) => {
-   try {
-      // this is not crashing but Idk if it actually gets anything useful
-      // Get yaks around given location, measured along the surface of Earth
-      const localYakList = await Yak.where("geometry").near({
-         center: {
-            type: "Point",
-            coordinates: [parseFloat(req.body.lng), parseFloat(req.body.lat)]
-         },
-         maxDistance: 8046.72, // 5 miles
-         spherical: true
-      });
+  try {
+    // this is not crashing but Idk if it actually gets anything useful
+    // Get yaks around given location, measured along the surface of Earth
+    const localYakList = await Yak.where("geometry").near({
+      center: {
+        type: "Point",
+        coordinates: [parseFloat(req.body.lng), parseFloat(req.body.lat)]
+      },
+      maxDistance: 8046.72, // 5 miles
+      spherical: true
+    });
 
-      // Respond with found yaks
-      res.status(200).json({ success: true, yaks: localYakList });
-   } catch (exception) {
-      console.log(exception);
-      res.status(400).json({ success: false, exception });
-   }
+    // Respond with found yaks
+    res.status(200).json({ success: true, yaks: localYakList });
+  } catch (exception) {
+    console.log(exception);
+    res.status(400).json({ success: false, exception });
+  }
 });
 
 // Posts a yak to logged-in user, broadcasts to others for
 router.post("/", auth, async (req, res) => {
-   try {
-      // Get User the logged in from DB
-      const user = await User.findById(req.user.id);
+  try {
+    // Get User the logged in from DB
+    const user = await User.findById(req.user.id);
 
-      // Creates and saves new Yak from input data
-      const newYak = new Yak({
-         content: req.body.content,
-         author: user.email,
-         geometry: {
-            type: "Point",
-            coordinates: [parseFloat(req.body.lng), parseFloat(req.body.lat)]
-         }
-      });
+    // Creates and saves new Yak from input data
+    const newYak = new Yak({
+      content: req.body.content,
+      author: user.email,
+      geometry: {
+        type: "Point",
+        coordinates: [parseFloat(req.body.lng), parseFloat(req.body.lat)]
+      }
+    });
 
-      await newYak.save();
+    await newYak.save();
 
-      // Add yak to User object
-      user.posts.push(newYak);
+    // Add yak to User object
+    user.posts.push(newYak);
 
-      // Save modified User object to DB
-      await user.save();
+    // Save modified User object to DB
+    await user.save();
 
-      // Respond with success
-      res.status(200).json({ success: true, newYak });
-   } catch (exception) {
-      console.log(exception);
-      res.status(400).json({ success: false, exception });
-   }
+    // Respond with success
+    res.status(200).json({ success: true, newYak });
+  } catch (exception) {
+    console.log(exception);
+    res.status(400).json({ success: false, exception });
+  }
+});
+
+router.delete("/", auth, async (req, res) => {
+  try {
+    // Find current user
+    const user = User.findById(req.user.id);
+
+    // Delete comments from Yak
+    // const yak = Yak.findById({id: req.params.id});
+    // yak.
+
+    // Delete yak and filter from user
+    await Yak.deleteOne({ id: req.params.id });
+
+    user.posts = user.posts.filter(yak => yak.id != req.params.id);
+
+    await user.save();
+    res.status(200).json({ success: true });
+  } catch (exception) {
+    console.log(exception);
+    res.status(400).json({ success: false, exception });
+  }
 });
 
 module.exports = router;
