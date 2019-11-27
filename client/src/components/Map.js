52078ccec64a7067278b89d51df7c7d0f06d2f59
John C <conjohnerton@gmail.com>
2019-11-27 16:07:43 -0500

    Map implemented and style, ANNOYINGLY, set... for now

---
+++ client/src/components/Map.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import MapGl, { Marker } from "react-map-gl";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+
+import keys from "../keys";
+
+const MapBox = (props) => {
+  const [state, setState] = useState({
+    viewport: {
+      width: 350,
+      height: 350,
+      latitude: props.location.lat,
+      longitude: props.location.lng,
+      zoom: 8
+    }
+  });
+  return (
+    <MapGl
+      mapStyle="mapbox://styles/mapbox/streets-v11"
+      mapboxApiAccessToken={keys.mapBox}
+      {...state.viewport}
+      onViewportChange={(viewport) => setState({ viewport })}
+    >
+      <Marker
+        offsetLeft={-20}
+        offsetTop={-10}
+        latitude={props.location.lat}
+        longitude={props.location.lng}
+      >
+        <div>You are here!</div>
+        <PersonPinIcon style={{ marginLeft: "1em" }} />
+      </Marker>
+    </MapGl>
+  );
+};
+
+export default MapBox;
