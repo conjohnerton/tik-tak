3c2499448eda2ac14fd3644c644d496ad7fa5470
John C <conjohnerton@gmail.com>
2019-12-15 22:35:52 -0500

    Users can only upvote a post one time per session.

--- client/src/components/UpvoteBox.js
+++ client/src/components/UpvoteBox.js
@@ -3,23 +3,32 @@ import { Typography, Button } from "@material-ui/core";
 import { ThumbUpAltRounded } from "@material-ui/icons";
 
 // The box that holds the upvote stuff in a YakCard
-const UpvoteBox = ({ upvotes, upvoteYak }) => (
-  <div
-    style={{
-      marginTop: "0.5em",
-      display: "flex",
-      justifyContent: "flex-end"
-    }}
-  >
-    <Typography style={{ paddingTop: ".5em" }} align="right" variant="body1">
-      {upvotes} upvotes
-    </Typography>
+const UpvoteBox = ({ upvotes, upvoteYak }) => {
+  // Upvote status gets reset every time app refreshes
+  const [upvoted, setUpvoted] = React.useState(false);
+  function submitUpvote() {
+    setUpvoted(true);
+    upvoteYak();
+  }
 
-    {/* Upvote button! */}
-    <Button onClick={upvoteYak}>
-      <ThumbUpAltRounded />
-    </Button>
-  </div>
-);
+  return (
+    <div
+      style={{
+        marginTop: "0.5em",
+        display: "flex",
+        justifyContent: "flex-end"
+      }}
+    >
+      <Typography style={{ paddingTop: ".5em" }} align="right" variant="body1">
+        {upvotes} upvotes
+      </Typography>
+
+      {/* Upvote button! */}
+      <Button disabled={upvoted} onClick={submitUpvote}>
+        <ThumbUpAltRounded />
+      </Button>
+    </div>
+  );
+};
 
 export default UpvoteBox;
